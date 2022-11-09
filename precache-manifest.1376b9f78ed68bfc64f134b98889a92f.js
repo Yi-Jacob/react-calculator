ba202c5b2c62c2158aede5fc31a0a366ae7beec8
Yi-Jacob <jakeyi00@gmail.com>
2022-11-08 19:05:36 -0800

    Updates

---
+++ precache-manifest.1376b9f78ed68bfc64f134b98889a92f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b404fb04617a942fcc428e4008ac846e",
+    "url": "/Yi-Jacob/react-calculator/index.html"
+  },
+  {
+    "revision": "7cf10d19f4e161e4bd81",
+    "url": "/Yi-Jacob/react-calculator/static/css/main.27353e52.chunk.css"
+  },
+  {
+    "revision": "ec8ef890e2dfe6ab7f36",
+    "url": "/Yi-Jacob/react-calculator/static/js/2.8e554d8d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Yi-Jacob/react-calculator/static/js/2.8e554d8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cf10d19f4e161e4bd81",
+    "url": "/Yi-Jacob/react-calculator/static/js/main.ab2a7d82.chunk.js"
+  },
+  {
+    "revision": "c09e81ded736b8d7ee75",
+    "url": "/Yi-Jacob/react-calculator/static/js/runtime-main.5e5b0326.js"
+  }
+]);
